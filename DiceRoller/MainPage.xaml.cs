f896b168e8f245a3150efdff8201cbfc67744589
luiz <luizfernando3995@gmail.com>
2024-04-01 22:00:30 -0300

    atividade corrigida

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -1,24 +1,30 @@
-﻿namespace DiceRoller
+﻿using Microsoft.Extensions.Options;
+
+namespace DiceRoller
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
+        int valor = 0;
         public MainPage()
         {
+
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void RollBtn_Clicked(object sender, EventArgs e)
         {
-            count++;
+            //Pegar o valor que foi selecionado pelo usuario no picker
+            valor = Convert.ToInt32( SidesPicker.SelectedItem);
+            //Jogar esse valor em uma variavel
+            //Sortear um número em um dado utilizando o Random
+            //Colocar o valor sorteado em uma variavel // OBS: Classe é um modelo/ideia e objeto é uma coisa
+            var umvalor = 0;
+            umvalor = new Random().Next(1, 10);  //ou Random.Shared.Next
+            ResultLabel.Text = umvalor.ToString();
+            //Exibir o valor para o usúario na  ResultLabel
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 
--- DiceRoller/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/DiceRoller.AssemblyInfo.cs
+++ DiceRoller/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/DiceRoller.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiceRoller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+84d00e871192301674fa59c104b3c2f35ebd952a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiceRoller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiceRoller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
